75a8e2b62cfb830656e1ebf6ce7176384385d08d
jyhwang <stratojy@gmail.com>
2025-05-07 00:23:01 +0900

    feat. 프로젝트 시작
    1. 스프링 부트 2.7.18 프로젝트 시작

---
+++ src/main/java/com/fastcampus/junitspringcalculator/JunitSpringCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.junitspringcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitSpringCalculatorApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JunitSpringCalculatorApplication.class, args);
+  }
+
+}
